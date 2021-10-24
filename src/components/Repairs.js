508590c6ef0e927770a57c9f0631da8538d37a26
Abdurehman05 <abdukr5@gmail.com>
2021-10-23 21:45:39 -0500

    useState and useEffect to pull api data json

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,5 +1,26 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 export const Repairs = () => {
-  return <h1>Honey Rae's Repair Shop </h1>;
+  const [customers, changeCustomers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8088/customers").then(res =>
+      res.json().then(customerArray => {
+        changeCustomers(customerArray);
+      })
+    );
+  }, []);
+  return (
+    <>
+      <h1>Honey Rae's Repair Shop </h1>
+      {customers.map(customerObject => {
+        return (
+          <h2>
+            {customerObject.name}
+            {/* {customerObject.email}
+            {customerObject.address} */}
+          </h2>
+        );
+      })}
+    </>
+  );
 };
