92b0cf4a5a43d6243640f5187f56354e3ccc7fbb
Abdurehman05 <abdukr5@gmail.com>
2021-10-25 05:20:32 -0500

    Customer List component added to follow Sigle Responsibility Principle

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,26 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { CustomerList } from "./customers/CustomerList";
 export const Repairs = () => {
-  const [customers, changeCustomers] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:8088/customers").then(res =>
-      res.json().then(customerArray => {
-        changeCustomers(customerArray);
-      })
-    );
-  }, []);
   return (
     <>
       <h1>Honey Rae's Repair Shop </h1>
-      {customers.map(customerObject => {
-        return (
-          <h2>
-            {customerObject.name}
-            {/* {customerObject.email}
-            {customerObject.address} */}
-          </h2>
-        );
-      })}
+      <CustomerList />
     </>
   );
 };
---
+++ src/components/customers/CustomerList.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+export const CustomerList = () => {
+  const [customers, changeCustomers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8088/customers").then(res =>
+      res.json().then(customerArray => {
+        changeCustomers(customerArray);
+      })
+    );
+  }, []);
+  return (
+    <>
+      {customers.map(customerObject => {
+        return (
+          <h3 key={`customer--${customerObject.id}`}>
+            {customerObject.name}
+            {/* {customerObject.email}
+            {customerObject.address} */}
+          </h3>
+        );
+      })}
+    </>
+  );
+};
